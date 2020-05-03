4cc282f35be690c12994a889971c2cb737bf69ce
Олег Витюк <oleg_valerievich@Olegs-MacBook-Pro.local>
2020-05-03 19:59:20 +0300

    Updates

---
+++ precache-manifest.278b0837e365d06d8fc67c8d6cda2a6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c34af8662ba647d863ce455a55ef14d",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "3a7a486de3b568a3248e",
+    "url": "/test1/static/css/main.7feeea0a.chunk.css"
+  },
+  {
+    "revision": "5898a631424c25a15703",
+    "url": "/test1/static/js/2.be034e3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test1/static/js/2.be034e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7a486de3b568a3248e",
+    "url": "/test1/static/js/main.f5f1a5dd.chunk.js"
+  },
+  {
+    "revision": "137befc3a870b543d8b6",
+    "url": "/test1/static/js/runtime-main.affb223a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test1/static/media/logo.5d5d9eef.svg"
+  }
+]);
